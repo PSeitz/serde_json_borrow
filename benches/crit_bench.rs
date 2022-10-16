dcd9bcf6b5e857999e1cb3d44c0edad0ef1ef51a
Pascal Seitz <pascal.seitz@gmail.com>
2022-10-17 00:44:49 +0800

    add bench
         Running benches/crit_bench.rs (/home/pascal/cargo_target_dir/release/deps/crit_bench-fd2d661e0b4255c5)
    simple-json-to-doc/serde-json-owned
                            time:   [361.19 µs 361.63 µs 362.30 µs]
                            thrpt:  [230.77 MiB/s 231.20 MiB/s 231.49 MiB/s]
                     change:
                            time:   [+1.4624% +1.6939% +1.8936%] (p = 0.00 < 0.05)
                            thrpt:  [-1.8584% -1.6656% -1.4414%]
                            Performance has regressed.
    Found 11 outliers among 100 measurements (11.00%)
      3 (3.00%) low mild
      4 (4.00%) high mild
      4 (4.00%) high severe
    simple-json-to-doc/serde-json-borrowed
                            time:   [280.87 µs 282.80 µs 284.62 µs]
                            thrpt:  [293.76 MiB/s 295.65 MiB/s 297.68 MiB/s]
                     change:
                            time:   [+4.4908% +5.0303% +5.5947%] (p = 0.00 < 0.05)
                            thrpt:  [-5.2982% -4.7894% -4.2978%]
                            Performance has regressed.
    Found 18 outliers among 100 measurements (18.00%)
      15 (15.00%) low severe
      3 (3.00%) low mild

---
+++ benches/crit_bench.rs
@@ -0,0 +1,33 @@
+use criterion::{criterion_group, criterion_main, Criterion, Throughput};
+
+const JSON_TEST_DATA: &str = include_str!("simple-parse-bench.json");
+
+pub fn simple_json_to_doc_benchmark(c: &mut Criterion) {
+    let lines: Vec<&str> = JSON_TEST_DATA.lines().map(|line| line.trim()).collect();
+
+    let mut group = c.benchmark_group("simple-json-to-doc");
+    group.throughput(Throughput::Bytes(JSON_TEST_DATA.len() as u64));
+    group.bench_function("serde-json-owned", |b| {
+        b.iter(|| {
+            let mut val = None;
+            for line in &lines {
+                let json: serde_json::Value = serde_json::from_str(line).unwrap();
+                val = Some(json);
+            }
+            val
+        })
+    });
+    group.bench_function("serde-json-borrowed", |b| {
+        b.iter(|| {
+            let mut val = None;
+            for line in &lines {
+                let json: serde_json_borrow::Value = serde_json::from_str(line).unwrap();
+                val = Some(json);
+            }
+            val
+        })
+    });
+}
+
+criterion_group!(benches, simple_json_to_doc_benchmark);
+criterion_main!(benches);
